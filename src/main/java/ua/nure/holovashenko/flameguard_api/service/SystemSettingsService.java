08ab8b2c8e1b242beea42eab7b079834650c5efc
w.t.f.is_it <illia.holovashenko@nure.ua>
2025-04-25 22:42:21 +0300

    separated FlameGuard API from arkpz-pzpi-22-6-holovashenko-illia into a separate repository

---
+++ src/main/java/ua/nure/holovashenko/flameguard_api/service/SystemSettingsService.java
@@ -0,0 +1,107 @@
+package ua.nure.holovashenko.flameguard_api.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.nure.holovashenko.flameguard_api.entity.SystemSettings;
+import ua.nure.holovashenko.flameguard_api.repository.SystemSettingsRepository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SystemSettingsService {
+
+    @Autowired
+    private SystemSettingsRepository systemSettingsRepository;
+
+    private final DataSource dataSource;
+
+    private static final Logger logger = LoggerFactory.getLogger(SystemSettingsService.class);
+
+    public SystemSettingsService(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<SystemSettings> getAllSettings() {
+        return systemSettingsRepository.findAll();
+    }
+
+    public Optional<SystemSettings> updateSettingValue(String key, String value) {
+        Optional<SystemSettings> settingOptional = systemSettingsRepository.findById(key);
+        if (settingOptional.isPresent()) {
+            SystemSettings setting = settingOptional.get();
+            setting.setSettingValue(value);
+            systemSettingsRepository.save(setting);
+            return Optional.of(setting);
+        }
+        return Optional.empty();
+    }
+
+    public void createDatabaseBackup(String backupFilePath) {
+        String backupQuery = String.format("BACKUP DATABASE FlameGuard TO DISK = '%s'", backupFilePath);
+
+        executeDatabaseOperation(backupQuery, "Backup created successfully", "Error creating database backup");
+    }
+
+    public void restoreDatabase(String backupFilePath) {
+        String switchToMasterQuery = "USE master;";
+        String setSingleUserQuery = "ALTER DATABASE FlameGuard SET SINGLE_USER WITH ROLLBACK IMMEDIATE;";
+        String restoreQuery = String.format("RESTORE DATABASE FlameGuard FROM DISK = '%s' WITH REPLACE", backupFilePath);
+        String setMultiUserQuery = "ALTER DATABASE FlameGuard SET MULTI_USER;";
+
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            statement.execute(switchToMasterQuery);
+            logger.info("Switched to master database.");
+
+            executeStatement(statement, setSingleUserQuery, "Database set to SINGLE_USER mode.");
+            executeStatement(statement, restoreQuery, "Database restored successfully.");
+            executeStatement(statement, setMultiUserQuery, "Database set to MULTI_USER mode.");
+
+        } catch (SQLException e) {
+            logger.error("Error restoring database: {}", e.getMessage(), e);
+            throw new RuntimeException("Error restoring database", e);
+        }
+    }
+
+    private void executeDatabaseOperation(String query, String successMessage, String errorMessage) {
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            statement.execute(query);
+            logger.info(successMessage);
+
+        } catch (SQLException e) {
+            logger.error("{}: {}", errorMessage, e.getMessage(), e);
+            throw new RuntimeException(errorMessage, e);
+        }
+    }
+
+    private void executeStatement(Statement statement, String query, String successMessage) throws SQLException {
+        statement.execute(query);
+        logger.info(successMessage);
+    }
+
+    public SystemSettings saveOrUpdateSetting(String settingKey, String settingValue) {
+        Optional<SystemSettings> existingSetting = systemSettingsRepository.findById(settingKey);
+
+        SystemSettings systemSettings = existingSetting.orElse(new SystemSettings());
+        systemSettings.setSettingKey(settingKey);
+        systemSettings.setSettingValue(settingValue);
+
+        return systemSettingsRepository.save(systemSettings);
+    }
+
+    public String getMeasurementsCheckInterval() {
+        return systemSettingsRepository.findById("measurements_check_interval")
+                .map(SystemSettings::getSettingValue)
+                .orElse("300000"); // Значення за замовчуванням: "1800000" мілісекунд (30 хвилин)
+    }
+}
